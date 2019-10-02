4a66e751642eb4435e9d7b85d9955eef5ea90c2e
Jakub Karolak <karolakjakub90@gmail.com>
2019-10-02 18:47:11 +0200

    php and html

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
-    echo 'Hello world'
+echo 'Hello world';
+echo 'aaaand second hello'
 
 ?>
+</body>
+</html>
