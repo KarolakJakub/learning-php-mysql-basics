e4c4ba3dfc69b31ba2e13aa9283ee3b5d35351a5
Jakub Karolak <karolakjakub90@gmail.com>
2019-10-08 14:22:46 +0200

    added advance form validation and showing errors

--- add.php
+++ add.php
@@ -1,37 +1,40 @@
 <?php
 
+
+
+$errors = array('email' => '', 'name' => '', 'ingredients' => '');
+
 if (isset($_POST['submit'])) {
-    // echo htmlspecialchars($_POST['email']);
-    // echo htmlspecialchars($_POST['name']);
-    // echo htmlspecialchars($_POST['ingridents']);
+
 
     if (empty($_POST['email'])) {
-        echo 'an email is requried <br />';
+        $errors['email'] = 'email is requried<br/>';
     } else {
         $email = $_POST['email'];
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            echo 'email is not vaild<br/>';
+            $errors['email'] = 'email is not vaild<br/>';
         }
     }
 
     if (empty($_POST['name'])) {
-        echo 'a name is requried <br />';
+        $errors['name'] = 'a name is requried <br />';
     } else {
         $name = $_POST['name'];
-        if(!preg_match('/^[a-zA-Z\s]+$/', $name)){
-            echo 'name must be letters only<br/>';
+        if (!preg_match('/^[a-zA-Z\s]+$/', $name)) {
+            $errors['name'] =  'name must be letters only<br/>';
         }
-       
     }
 
-    if (empty($_POST['ingridents'])) {
-        echo 'ingridents are requried <br />';
+    if (empty($_POST['ingredients'])) {
+        $errors['ingredients'] = 'ingredients are requried <br />';
     } else {
-        $ingridents = $_POST['ingridents'];
-        if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingridents)){
-            echo 'ingridents must be letters only and comma separeted<br/>';
+        $ingredients = $_POST['ingredients'];
+        if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+            $errors['ingredients'] = 'ingredients must be letters only and comma separeted<br/>';
         }
     }
+} else {
+    $name = $email = $ingredients = '';
 }
 
 ?>
@@ -48,11 +51,14 @@ if (isset($_POST['submit'])) {
         <h4 class="center">Add a Pizza</h4>
         <form action="" class="white" action="add.php" method='POST'>
             <label>Your Email:</label>
-            <input type='text' name='email'>
+            <input type='text' name='email' value='<?php echo $email ?>'>
+            <div class="red-text"><?php echo $errors['email'] ?></div>
             <label>Pizza name:</label>
-            <input type='text' name='name'>
+            <input type='text' name='name' value='<?php echo $name ?>'>
+            <div class="red-text"><?php echo $errors['name'] ?></div>
             <label>Ingridents:</label>
-            <input type='text' name='ingridents'>
+            <input type='text' name='ingredients' value='<?php echo $ingredients ?>'>
+            <div class="red-text"><?php echo $errors['ingredients'] ?></div>
             <div class="center">
                 <input type="submit" name="submit" value="submit" class="btn">
             </div>
