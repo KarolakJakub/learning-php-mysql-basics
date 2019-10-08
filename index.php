dd23431cf7b8d317ae6629a123816e2e216b6c13
Jakub Karolak <karolakjakub90@gmail.com>
2019-10-08 11:56:20 +0200

    header and footer for the pizza portal

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
-<?php
-
-include 'users.php';
-require 'users.php';
-
-echo 'EoF';
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
 <body>
 
-<?php include 'content.php'?>
+    <?php include 'templates/header.php' ?>
+    <?php include 'templates/footer.php' ?>
 
 </body>
 
---
+++ templates/header.php
@@ -0,0 +1,15 @@
+<head>
+    <title>Pizza Portal</title>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+</head>
+
+<body class='red lighten-4'>
+    <nav class='grey z-depth-1'>
+        <div class='container'>
+            <a href="#" class='brand-logo brand-text'>Pizza Portal</a>
+            <ul id='nav-mobile' class='right hide-on-small-and-down'>
+                <li><a href="#" class="btn">Add a Pizza</a></li>
+            </ul>
+        </div>
+    </nav>
