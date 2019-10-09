900c5185e7451e039cc6b03d56c4147d3cff77fb
Jakub Karolak <karolakjakub90@gmail.com>
2019-10-09 11:12:12 +0200

    added delete button and funcionality

--- details.php
+++ details.php
@@ -2,6 +2,19 @@
 
 include('config/db_connect.php');
 
+if (isset($_POST['delete'])) {
+
+    $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+    $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: index.php");
+    } else {
+        echo mysqli_error($conn);
+    }
+}
+
 if (isset($_GET['id'])) {
 
     $id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -33,11 +46,15 @@ if (isset($_GET['id'])) {
                     <li><?php echo htmlspecialchars($label . " - " . $val) ?></li>
 
                 <?php endforeach; ?>
+                <form action="details.php" method="POST">
+                    <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id'] ?>">
+                    <input type="submit" name="delete" value="Delete" class="btn">
+                </form>
             </ul>
         </h4>
     <?php else : ?>
 
-    <h4>Pizza doesn't exist</h4>
+        <h4>Pizza doesn't exist</h4>
 
     <?php endif; ?>
 
